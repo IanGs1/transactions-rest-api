94b930950d21ad62fe343a76dd56e0e1d58c4f4c
Ian <goldfeld35@gmail.com>
2024-02-28 21:58:10 -0300

    config: knex migrations using typescript

--- src/database/knex/index.ts
+++ src/database/knex/index.ts
@@ -1,10 +1,17 @@
 import path from "node:path";
 
-import { knex as setupKnex } from "knex";
+import { Knex, knex as setupKnex } from "knex";
 
-export const knex = setupKnex({
+export const config: Knex.Config = {
   client: "sqlite3",
   connection: {
     filename: path.resolve(__dirname, "..", "database.db"),
-  }
-});
+  },
+  migrations: {
+    directory: path.resolve(__dirname, ".", "migrations"),
+    extension: "ts",
+  },
+  useNullAsDefault: true,
+}
+
+export const knex = setupKnex(config);
