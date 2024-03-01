a7a1381438ed2af6e3080c6c2e5a2d60ff433f86
Ian <goldfeld35@gmail.com>
2024-02-29 21:21:48 -0300

    feat: create transactions table

---
+++ src/database/knex/migrations/20240301001015_alter-transactions.ts
@@ -0,0 +1,20 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  knex.schema.alterTable("transactions", (table) => {
+    table.decimal("amount", 10, 2).notNullable();
+    table.uuid("session_id").after("id").index();
+
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  knex.schema.alterTable("transactions", (table) => {
+    table.dropColumn("amount");
+    table.dropColumn("session_id");
+
+    table.dropColumn("created_at");
+  })
+}
+
