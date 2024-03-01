47565df4171e8783c80d1fd1614a8a22d89316dc
Ian <goldfeld35@gmail.com>
2024-02-29 22:02:53 -0300

    feat: working with enviroment variables

--- knexfile.ts
+++ knexfile.ts
@@ -1,10 +1,11 @@
 import { Knex } from "knex";
 
 import path from "node:path";
+import env from "./src/env";
 
 export = {
   client: "sqlite3",
-  connection: path.resolve(__dirname, "src", "database", "database.db"),
+  connection: env.DATABASE_URL,
 
   migrations: {
     directory: path.resolve(__dirname, "src", "database", "knex", "migrations"),
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import fastify from "fastify";
 import crypto from "node:crypto"
 
 import knex from "./database/knex";
+import env from "./env";
 
 const app = fastify();
 
@@ -17,7 +18,7 @@ app.post("/transactions", async (request, reply) => {
 })
 
 app
-  .listen({ port: 3333 })
+  .listen({ port: env.PORT })
   .then(() => {
     console.log("HTTP server listening on: localhost:3333 ✨");
   })
